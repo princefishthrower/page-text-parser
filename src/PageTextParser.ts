41e205ada507c9cc7d7ee6cabb689a0c49184493
Chris Frewin <frewin.christopher@gmail.com>
2020-06-23 10:16:09 +0200

    beefed up tsconfig.json

--- src/PageTextParser.ts
+++ src/PageTextParser.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 
 export default class PageTextParser {
   public static async parse(url: string, selector: string): Promise<Array<string>> {
-    let results = [];
+    let results: Array<string> = [];
     try {
       const response = await axios.get(url);
       const $ = cheerio.load(response.data);
-      $(selector).each(function (index, element) {
+      $(selector).each(function (index: number, element: any) {
         results.push($(element).text());
       });
     } catch (e) {
